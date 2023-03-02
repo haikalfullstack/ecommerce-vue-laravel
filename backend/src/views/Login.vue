22a11cb5410fab13963c831c730db35cc988d5de
haikalrozhan <haikalrozhan1992@yahoo.com>
2023-03-03 03:00:52 +0800

    create request password reset and reset password pages

--- backend/src/views/Login.vue
+++ backend/src/views/Login.vue
@@ -4,13 +4,9 @@
   <div class="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
     <div class="w-full max-w-md space-y-8">
       <div>
-        <img class="mx-auto h-12 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="Your Company" />
+     
         <h2 class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Sign in to your account</h2>
-        <p class="mt-2 text-center text-sm text-gray-600">
-          Or
-          {{ ' ' }}
-          <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">start your 14-day free trial</a>
-        </p>
+      
       </div>
       <form class="mt-8 space-y-6" action="#" method="POST">
         <input type="hidden" name="remember" value="true" />
@@ -32,7 +28,7 @@
           </div>
 
           <div class="text-sm">
-            <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">Forgot your password?</a>
+            <router-link :to="{name: 'requestPassword'}" class="font-medium text-indigo-600 hover:text-indigo-500">Forgot your password?</router-link>
           </div>
         </div>
 
---
+++ backend/src/views/ResetPassword.vue
@@ -0,0 +1,57 @@
+
+<template>
+
+  <div class="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+    <div class="w-full max-w-md space-y-8">
+      <div>
+     
+        <h2 class="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Set New Password</h2>
+      
+      </div>
+      <form class="mt-8 space-y-6" action="#" method="POST">
+        <input type="hidden" name="remember" value="true" />
+        <div class="-space-y-px rounded-md shadow-sm">
+          <div>
+           
+            <input id="new-password" name="password" type="password"  required="" class="relative block w-full rounded-t-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="New Password" />
+          </div>
+          <div>
+        
+            <input id="password-repeat" name="password_repeat" type="password"  required="" class="relative block w-full rounded-b-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Repeat Password" />
+          </div>
+        </div>
+
+        <div class="flex items-center justify-between">
+         
+
+          <div class="text-sm">
+            <router-link :to="{name: 'login'}" class="font-medium text-indigo-600 hover:text-indigo-500">Go back to login</router-link>
+          </div>
+        </div>
+
+        <div>
+          <button type="submit" class="group relative flex w-full justify-center rounded-md bg-indigo-600 py-2 px-3 text-sm font-semibold text-white hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+            <span class="absolute inset-y-0 left-0 flex items-center pl-3">
+              <LockClosedIcon class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" aria-hidden="true" />
+            </span>
+            Submit
+          </button>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { LockClosedIcon } from '@heroicons/vue/20/solid'
+</script>
+
+<script>
+    export default{
+        name: 'ResetPassword'
+    }
+</script>
+
+<style scoped>
+
+</style>
